055ffe1e5a65c5024e1e00ea694873dfbcaea95f
jcontheweb <cairns.webdev@gmail.com>
2020-12-15 13:36:59 +0000

    dynamically import page sections

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import { useEffect } from 'react'
+import dynamic from 'next/dynamic'
 import "tailwindcss/tailwind.css";
 import "../assets/style.css"
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Navigation from "../components/Navigation/Navigation";
-import Footer from "../components/Footer/Footer";
-import SocialBar from "../components/SocialBar/SocialBar";
+
+// import Navigation from "../components/Navigation/Navigation";
+// import Footer from "../components/Footer/Footer";
+// import SocialBar from "../components/SocialBar/SocialBar";
+
+const Navigation = dynamic(import('../components/Navigation/Navigation'))
+const Footer = dynamic(import('../components/Footer/Footer'))
+const SocialBar = dynamic(import('../components/SocialBar/SocialBar'))
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
