b9dddb3c0f8597894b4d462c2ccf340e7c46d3d1
jcontheweb <cairns.webdev@gmail.com>
2020-12-15 14:59:38 +0000

    fix broken links

--- components/Sections/About.js
+++ components/Sections/About.js
@@ -6,7 +6,7 @@ export default function AboutSection() {
       style={{ marginTop: "100vh" }}
       className="bg-white z-10 relative py-8 md:py-0 md:pt-16 flex items-center"
     >
-      <a id="about" href="javascript:void(0)" className="sr-only" style={{ top: "-79px" }}></a>
+      <span id="about" className="sr-only" style={{ top: "-79px" }}></span>
       <div className="max-w-5xl w-full mx-auto px-4 md:px-8">
         <AboutTabs />
       </div>
